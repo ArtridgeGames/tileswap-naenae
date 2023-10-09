54173a316844952b5ed2a953037e4ddd4cfd8232
Oskar <oskar.codes@gmail.com>
2023-10-09 15:44:20 +0200

    brought back horror

--- src/assets/js/Layout.js
+++ src/assets/js/Layout.js
@@ -100,7 +100,7 @@ export class Layout {
   }).sort((a, b) => a.unlockCategory - b.unlockCategory);
 
   static HIDDEN_LAYOUTS = [
-    { dimensions: "4x4", exclude: [1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14], unlockCategory: -2, id: 32 }, {
+    { dimensions: "4x4", exclude: [1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14], unlockCategory: 0, id: 32 }, {
       dimensions: "5x5",
       exclude: [5, 7, 9, 10, 12, 14, 15, 17, 19],
       unlockCategory: -2,
