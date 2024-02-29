288dd811f71fbcb13c03dd9956cb67e30e4bfc30
Oskar Codes <oskar.codes@gmail.com>
2024-02-29 12:50:21 +0000

    introview

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'intro',
-      component: TutorialViewAdvanced
+      component: IntroView // TutorialViewAdvanced
     },
     {
       path: '/home',
