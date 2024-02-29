5fc3bf1729e0280380ddb8e2ce1218afcf009eb7
Oskar <oskar.codes@gmail.com>
2024-02-29 10:59:29 +0100

    Rudimentary tutorial

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'intro',
-      component: IntroView
+      component: TutorialViewAdvanced
     },
     {
       path: '/home',
--- src/views/TutorialViewAdvanced.vue
+++ src/views/TutorialViewAdvanced.vue
@@ -15,19 +15,46 @@ import Button from '../components/buttons/Button.vue';
         <Button black text="ok!" @pressed="showModal = false" />
       </div>
       <div v-if="modalPage == 1">
-        <h1>Now see what happens when tiles are missing.</h1>
+        <h1>You've won!</h1>
+        <h3>Your'e free to continue experimenting. Continue when you're ready.</h3>
+        <Button black text="ok!" @pressed="showModal = false" />
       </div>
       <div v-if="modalPage == 2">
+        <h1>Now see what happens when tiles are missing.</h1>
+        <Button black text="ok!" @pressed="showModal = false" />
+      </div>
+      <div v-if="modalPage == 3">
         <h1>You should be getting the hang of it</h1>
+        <h1>Let's make this a bit interesting</h1>
+        <Button black text="ok!" @pressed="showModal = false" />
+      </div>
+      <div v-if="modalPage == 4">
+        <h1>Everything so far could be solved in a single click</h1>
+        <h1>However as you might have seen when experimenting, things get complicated very fast</h1>
+        <h1>For example when 2 clicks overlap</h1>
+        <h1>[INSERT ANIMATION]</h1>
+        <Button black text="ok!" @pressed="showModal = false" />
       </div>
     </Modal>
 
-    <LayoutVue 
+    <!-- <LayoutVue 
       class="center middle"
-      v-model="layout"
+      v-model="stage.layout"
+    /> -->
+
+    <h1 class="text-center" :class="{ shake }" v-if="showWrong">Wrong tile!</h1>
+
+    <LayoutVue
+      class="center middle"
+      v-model="stage.layout"
+      :class="{ shake }"
+      :disabled="disabled"
+      @swap="handleClick"
     />
 
     <p class="top center">{{ text }}</p>
+
+    <Button class="bottom center" v-if="enableNext" text="Next" @pressed="next" />
   </main>
 </template>
 
@@ -46,13 +73,14 @@ import { Layout } from '../assets/js/Layout.js';
 export default {
   data() {
 
-    const layouts = [
+    const stages = [
         {
           matrix: [[1,1,1,1,1],
                    [1,0,0,0,1],
                    [1,0,0,0,1],
                    [1,0,0,0,1],
                    [1,1,1,1,1]],
+          solution: [12],
         },
         {
           matrix: [[0,0,0,1,1],
@@ -60,6 +88,7 @@ export default {
                    [0,0,0,1,1],
                    [1,1,1,1,1],
                    [1,1,1,1,1]],
+          solution: [6]
         },
         {
           matrix: [[0,0,0,1,1,1],
@@ -68,6 +97,7 @@ export default {
                    [1,1,1,0,0,0],
                    [1,1,1,0,0,0],
                    [1,1,1,0,0,0]],
+          solution: [7,28]
         },
         {
           matrix: [[0,0,0,1,1,1,1],
@@ -77,6 +107,7 @@ export default {
                    [1,0,0,0,1,1,1],
                    [1,0,0,0,1,1,1],
                    [1,0,0,0,1,1,1]],
+          solution: [8,19,37]
         },
         {
           matrix: [[1,1,1,1,0,0,0],
@@ -196,25 +227,31 @@ export default {
                    [0,1,0],
                    [0,0,1]],
         }
-      ].map(layout => {
+      ].map(stage => {
         const res = new Layout({
-          width: layout.matrix[0].length,
-          height: layout.matrix.length,
-          exclude: Layout.getExcludeFromMatrix(layout.matrix),
+          width: stage.matrix[0].length,
+          height: stage.matrix.length,
+          exclude: Layout.getExcludeFromMatrix(stage.matrix),
         });
-        res.setMatrix(layout.matrix);
-        return res;
+        res.setMatrix(stage.matrix);
+        return {
+          layout: res,
+          showModal: stage.showModal,
+          solution: stage.solution,
+          modulo: stage.modulo,
+        }
       });
-
-    const layout = layouts[0];
 
     return {
       showModal: true,
       text: 'Try it here!',
-      modalPage: 0,
-      layoutIndex: 0,
-      layout,
-      layouts,
+      modalPage: -1,
+      stageIndex: 0,
+      stages,
+      shake: false,
+      showWrong: false,
+      disabled: undefined,
+      enableNext: false,
       exampleLayout: (() => {
         const layout =  new Layout({
           width: 5,
@@ -227,14 +264,60 @@ export default {
     }
   },
   watch: {
-    showModal() {
-      this.modalPage++;
+    showModal: {
+      handler(newVal) {
+        if (newVal) this.modalPage++;
+      },
+      immediate: true
     }
   },
   mounted() {
     window.setInterval(() => {
       this.exampleLayout.swapTiles(2, 2, 1, 2, tilesToFlip.value);
     }, 2e3);
+  },
+  computed: {
+    stage() {
+      return this.stages[this.stageIndex];
+    }
+  },
+  methods: {
+    handleClick(tileIndex, row, tile) {
+      if (this.stage.solution && !this.disabled && !this.enableNext) {
+        if (!this.stage.solution.includes(tileIndex)) {
+          this.disabled = true;
+          setTimeout(() => {
+            this.stage.layout.swapTiles(row, tile, -1, this.stage.modulo ?? 2);
+            this.shake = true
+            this.showWrong = true;
+            this.disabled = undefined;
+            setTimeout(() => {
+              this.showWrong = false;
+            }, 2e3);
+            setTimeout(() => {
+              this.shake = false
+            }, 900);
+          }, 500);
+          return;
+        } else {
+          this.stages[this.stageIndex].solution.splice(this.stages[this.stageIndex].solution.indexOf(tileIndex), 1);
+        }
+      }
+
+      if (this.stage.layout.isSolved(this.stage.modulo ?? 2) && !this.enableNext) {
+        if (this.modalPage === 0) {
+          this.showModal = true;
+        }
+        this.enableNext = true;
+      }
+    },
+    next() {
+      if (this.stage.showModal) {
+        this.showModal = true;
+      }
+      this.enableNext = false;
+      this.stageIndex++;
+    }
   }
 }
 </script>
