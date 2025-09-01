99fb6bb471d0a4df6746f4f0dc9da429d429de14
Redd87 <eddy.p.rashed@gmail.com>
2025-09-01 12:44:18 +0200

    add score animation to progress component

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -19,7 +19,7 @@
   width: v-bind(progress);
   height: 100%;
   background: v-bind(barColorOrDefault);
-  transition: width 0.5s ease-in-out;
+  transition: width 0.5s ease;
 }
 .container > .text {
   position: absolute;
@@ -33,14 +33,58 @@
 
 <script>
 export default {
-  props: ['value', 'max', 'text', 'barColor', 'backgroundColor'],
+  props: ['value', 'max', 'text', 'barColor', 'backgroundColor', 'startValue', 'duration'],
+  data() {
+    return {
+      animatedValue: this.startValue ?? this.value
+    }
+  },
+  watch: {
+    value(newValue) {
+      if (this.startValue !== undefined) {
+        this.animateToValue(newValue);
+      } else {
+        this.animatedValue = newValue;
+      }
+    }
+  },
   computed: {
     progress() {
       if (this.max == 0) return "100%";
-      return (this.value / this.max * 100) + "%";
+      return (this.animatedValue / this.max * 100) + "%";
     },
     barColorOrDefault() {
       return this.barColor ?? 'var(--root-bg-color)';
+    }
+  },
+  methods: {
+    animateToValue(targetValue) {
+      const startValue = this.animatedValue;
+      const duration = this.duration ?? 500;
+      const startTime = performance.now();
+      
+      const animate = (currentTime) => {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / duration, 1);
+        
+        // Easing function (ease-out)
+        const easedProgress = 1 - Math.pow(1 - progress, 3);
+        
+        this.animatedValue = startValue + (targetValue - startValue) * easedProgress;
+        
+        if (progress < 1) {
+          requestAnimationFrame(animate);
+        } else {
+          this.animatedValue = targetValue;
+        }
+      };
+      
+      requestAnimationFrame(animate);
+    }
+  },
+  mounted() {
+    if (this.startValue !== undefined && this.startValue !== this.value) {
+      this.animateToValue(this.value);
     }
   }
 }
