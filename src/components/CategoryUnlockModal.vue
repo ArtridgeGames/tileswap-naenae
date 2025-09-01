75f17aafb6a4192c4881cad2d60fa129a4446c75
Redd87 <eddy.p.rashed@gmail.com>
2025-09-01 11:46:45 +0200

    add unlocked challenges names

--- src/components/CategoryUnlockModal.vue
+++ src/components/CategoryUnlockModal.vue
@@ -1,7 +1,7 @@
 <template>
   <Modal :modelValue="modelValue" @update:modelValue="$emit('update:modelValue', $event)">
     <h1>Category {{ currentCategory + 1 }} Unlocked!</h1>
-    <h2>{{ newLayoutsCount }} New Layouts Unlocked!</h2>
+    <h1>{{ newLayoutsCount }} New Layouts Unlocked!</h1>
     <!-- <div class="layouts-grid">
       <LayoutPreview 
         v-for="layout in unlockedLayouts" 
@@ -10,9 +10,13 @@
         :size="80"
       />
     </div> -->
-    <h2>{{ newPuzzlesCount }} New Puzzles Unlocked!</h2>
-    <h2>{{ newChallengesCount }} New Challenges Unlocked!</h2>
-    
+    <h1>{{ newPuzzlesCount }} New Puzzles Unlocked!</h1>
+    <h1>{{ newChallengesCount }} New Challenges Unlocked!</h1>
+    <ul>
+        <li v-for="message in newChallengesNames" :key="message">
+        {{ message }}
+        </li>
+    </ul>
     <slot></slot>
   </Modal>
 </template>
@@ -48,11 +52,16 @@ export default {
       const categoryPuzzles = Puzzle.CATEGORIES[this.currentCategory];
       return categoryPuzzles ? categoryPuzzles.length : 0;
     },
+    categoryChallenges() {
+    return CHALLENGES.filter(
+      challengeGroup => challengeGroup.unlockCategory === this.currentCategory
+        );
+    },
+    newChallengesNames() {
+        return this.categoryChallenges.map(challenge => challenge.title);
+    },
     newChallengesCount() {
-      const categoryChallenges = CHALLENGES.filter(
-        challengeGroup => challengeGroup.unlockCategory === this.currentCategory
-      );
-      return categoryChallenges.reduce((total, group) => total + group.challenges.length, 0);
+      return this.newChallengesNames.length;
     }
   }
 }
